df7c2310fb03f669a73a135dbb6a2d328ad197f7
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2022-08-21 19:15:19 +0530

    Add Data set Tranning Machine learning Model

---
+++ web application/app.py
@@ -0,0 +1,69 @@
+from flask import Flask, render_template, request
+import pickle
+import numpy as np
+
+# setup application
+app = Flask(__name__)
+
+def prediction(lst):
+    filename = 'model/predictor.pickle'
+    with open(filename, 'rb') as file:
+        model = pickle.load(file)
+    pred_value = model.predict([lst])
+    return pred_value
+
+@app.route('/', methods=['POST', 'GET'])
+def index():
+    # return "Hello World"
+    pred_value = 0
+    if request.method == 'POST':
+        ram = request.form['ram']
+        weight = request.form['weight']
+        company = request.form['company']
+        typename = request.form['typename']
+        opsys = request.form['opsys']
+        cpu = request.form['cpuname']
+        gpu = request.form['gpuname']
+        touchscreen = request.form.getlist('touchscreen')
+        ips = request.form.getlist('ips')
+        
+        feature_list = []
+
+        feature_list.append(int(ram))
+        feature_list.append(float(weight))
+        feature_list.append(len(touchscreen))
+        feature_list.append(len(ips))
+
+        company_list = ['acer','apple','asus','dell','hp','lenovo','msi','other','toshiba']
+        typename_list = ['2in1convertible','gaming','netbook','notebook','ultrabook','workstation']
+        opsys_list = ['linux','mac','other','windows']
+        cpu_list = ['amd','intelcorei3','intelcorei5','intelcorei7','other']
+        gpu_list = ['amd','intel','nvidia']
+
+        # for item in company_list:
+        #     if item == company:
+        #         feature_list.append(1)
+        #     else:
+        #         feature_list.append(0)
+
+        def traverse_list(lst, value):
+            for item in lst:
+                if item == value:
+                    feature_list.append(1)
+                else:
+                    feature_list.append(0)
+        
+        traverse_list(company_list, company)
+        traverse_list(typename_list, typename)
+        traverse_list(opsys_list, opsys)
+        traverse_list(cpu_list, cpu)
+        traverse_list(gpu_list, gpu)
+
+        pred_value = prediction(feature_list)
+        pred_value = np.round(pred_value[0],2)*221
+
+    return render_template('index.html', pred_value=pred_value)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
